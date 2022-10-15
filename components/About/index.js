50687efe3279ea12a4abe8a36b5b274a756aed8d
Leinad <danielli12750@gmail.com>
2022-10-15 21:41:13 +1100

    changed about page

--- components/About/index.js
+++ components/About/index.js
@@ -18,19 +18,16 @@ const About = () => {
             >
               <div className={styles.about__info}>
                 <p>
-                  Hi, my name is Daniel Li and i'm self-taught web developer.
-                  I'm most focused on frontend, but i know some backend as well
-                  such as node, express and mongodb. On the frontend, i know
-                  HTML, CSS, JavaScript, React and Next.
+                  Hello, my name is Daniel Li. Ever since I was a teenager I’ve
+                  been passionate about programming. I love building projects
+                  and seeing my vision come to life. I want to be able turn this
+                  passion into something I can do for the rest of my life.
                 </p>
                 <p>
-                  I've been programming since 2019, but really it started even
-                  before that, when i was child playing Warcraft 3. i loved
-                  playing around in the map editor and experimenting with the
-                  triggers (a GUI language they had inside). this appears to
-                  this day as i love experimenting with the intricacies of
-                  languages and frameworks and i'm very passionate and curious
-                  about learning new technologies
+                  I’m a frontend devloper, but I know backend stuff as well. I
+                  have a strong understanding of HTML, CSS, Javascript, PHP and
+                  React. I am highly passionate about learning and love learning
+                  about new tools as well as building on my fundalmentals.
                 </p>
               </div>
             </div>
